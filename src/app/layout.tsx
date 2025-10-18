317ef3dfd0dd8dfdd87bae31088cf574595f594e
infan11 <infanjiounrahman20606@gmail.com>
2025-10-18 12:31:11 +0600

    added splashcursor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from 'react-hot-toast';
+import SplashCursor from './Components/SplashCursor/SplashCursor';
+
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,9 +30,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-poppins bg-dark`}
       >
+
+
+        <SplashCursor />
         {children}
+        <Toaster />
+
+
       </body>
     </html>
   );
