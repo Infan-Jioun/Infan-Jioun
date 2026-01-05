5802328de14e199673c94844a36994a03e2eb708
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-05 23:27:34 +0600

    Add sitemap and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://infan-portfolio.web.app", // তোমার deployed site URL
+  siteUrl: "https://infan-jioun.vercel.app/", // তোমার deployed site URL
   generateRobotsTxt: true, // robots.txt বানাবে
   changefreq: "daily",
   priority: 0.7,
