627627d574252c61b793e06f08bdbbe339a6ea8a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-09-15 20:58:07 +0600

    feat:

--- src/app/NavbarPage/page.tsx
+++ src/app/NavbarPage/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
-import { app } from "../../lib/firebase"; 
 
 export default function NavbarPage() {
     const [scrolled, setScrolled] = useState(false);
